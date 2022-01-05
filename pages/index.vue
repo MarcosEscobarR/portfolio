2e7d536a6813f0dee152945c8ba196d24dcfa841
MarcosEscobarR <64100957+MarcosEscobarR@users.noreply.github.com>
2022-01-05 14:40:38 -0300

    .

--- pages/index.vue
+++ pages/index.vue
@@ -13,15 +13,6 @@
 </template>
 <script>
 export default {
-  head () {
-    return {
-      meta: [
-        { hid: 'fb:app_id', name: 'fb:app_id', content: '12873892173892' },
-        { hid: 'og:title', name: 'og:title', content: 'test' },
-        { hid: 'og:image', name: 'og:image', content: '/favicon.io' }
-      ]
-    }
-  }
 }
 </script>
 <style scoped>
